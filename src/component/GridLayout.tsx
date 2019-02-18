3112920becc4ae255428eaed5aaefae2e1c20dbf
wz2cool <wz2cool@live.cn>
2019-02-18 19:59:22 +0800

    add

--- src/component/GridLayout.tsx
+++ src/component/GridLayout.tsx
@@ -18,12 +18,14 @@ export class MyFirstGrid extends Component<any, MyFirstGridState> {
   }
 
   private addNew = () => {
-    const layout = [
-      { i: "a", x: 0, y: 0, w: 1, h: 2 },
-      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-      { i: "c", x: 4, y: 0, w: 1, h: 2 },
-      { i: "d", x: 5, y: 0, w: 2, h: 2 }
-    ];
+    const layout = Object.assign([], this.state.layout);
+    layout.push({
+      i: new Date().getTime().toString(),
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 20
+    });
 
     this.setState({ layout: layout });
   };
