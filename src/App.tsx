a44e9275035d7c262449691584484f80b0986aa7
wz2cool <wz2cool@live.cn>
2019-02-19 11:34:11 +0800

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import "./App.css";
 import "../node_modules/react-grid-layout/css/styles.css";
 import "../node_modules/react-resizable/css/styles.css";
 import { MyFirstGrid } from "./component/MyFirstGrid";
+import { ResponsiveGrid } from "./component/ResponsiveGrid";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MyFirstGrid />
+        <ResponsiveGrid />
       </div>
     );
   }
---
+++ src/component/ResponsiveGrid.tsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { Responsive, WidthProvider, Layouts, Layout } from "react-grid-layout";
+import { ObjectUtils } from "ts-commons";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+interface ResponsiveGridState {
+  layouts: Layouts;
+}
+
+export class ResponsiveGrid extends Component<any, ResponsiveGridState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      layouts: this.getFromLS("layouts") || {}
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.resetLayout()}>Reset Layout</button>
+        <ResponsiveGridLayout
+          className="layout"
+          layouts={this.state.layouts}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          onLayoutChange={(layout, layouts) => {
+            this.onLayoutChange(layout, layouts);
+          }}
+        >
+          <div key="1" data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}>
+            <span className="text">1</span>
+          </div>
+          <div key="2" data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}>
+            <span className="text">2</span>
+          </div>
+          <div key="3" data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}>
+            <span className="text">3</span>
+          </div>
+          <div key="4" data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}>
+            <span className="text">4</span>
+          </div>
+          <div key="5" data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}>
+            <span className="text">5</span>
+          </div>
+        </ResponsiveGridLayout>
+      </div>
+    );
+  }
+
+  private resetLayout = () => {
+    this.setState({ layouts: {} });
+  };
+
+  private onLayoutChange(layout: Layout[], layouts: Layouts) {
+    this.saveToLS("layouts", layouts);
+    this.setState({ layouts });
+  }
+
+  private addNew = () => {};
+
+  private getDefaultItemLayouts(): Layout[] {
+    return [
+      { i: "1", w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 },
+      { i: "2", w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 },
+      { i: "3", w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 },
+      { i: "4", w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 },
+      { i: "5", w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }
+    ];
+  }
+
+  private getFromLS = (key: string): any => {
+    let ls: any = {};
+    if (localStorage) {
+      try {
+        const localData = localStorage.getItem("rgl-8");
+        if (!ObjectUtils.isNullOrUndefined(localData)) {
+          ls = JSON.parse(localData!) || {};
+        }
+      } catch (e) {
+        /*Ignore*/
+      }
+    }
+    return ls[key];
+  };
+
+  private saveToLS = (key: string, value: Layouts): void => {
+    if (localStorage) {
+      localStorage.setItem("rgl-8", JSON.stringify({ [key]: value }));
+    }
+  };
+}
